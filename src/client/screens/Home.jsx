883112b56b953a8006338862d4ab4a711abd794e
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-06-13 18:50:33 -0600

    home page: introduction added

--- src/client/screens/Home.jsx
+++ src/client/screens/Home.jsx
@@ -4,6 +4,15 @@ function Home() {
     return (
         <div>
             <h1>Home Page</h1>
+            <p>
+                Glenn Dutton specializes in personal, family, and organizational relationships. He holds a Masters degree in Counseling, a B.S. degree in Psychology, is a certified black belt in Aikido, instructor and holds certification in the Aiki Works Approach.
+            </p>
+            <p>
+                His gift and passion for seeing into and through issues led him to design and launch a behavioral management system for a leading health care corporation. Glenn’s program also revolutionized and increased the client satisfaction rate by 76% within the first 12 months of operation. 
+            </p>
+            <p>
+                As a personal coach, speaker and facilitator, Glenn draws on over 25 years of extensive experience. He has the ability to deliver key insights into relational dynamics while offering a hands-on experience which is entertaining, motivating, and distinctly empowering.
+            </p>
         </div>
     )
 }
