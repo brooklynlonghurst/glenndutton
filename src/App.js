b024018132466e135d84a8d25113eb2ece03c8a4
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-06-14 19:10:44 -0600

    nav bar functional

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css'
-
+import { Routes, Switch, Route } from 'react-router-dom'
+import Home from './client/screens/Home';
+import Testimonials from './client/screens/Testimonials';
+import Clients from './client/screens/Clients';
+import Contact from './client/screens/Contact';
 import Header from './client/components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/testimonials' element={<Testimonials />} />
+            <Route path='/clients' element={<Clients />} />
+            <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../screens/Home'
-import Testimonials from '../screens/Testimonials'
-import Clients from '../screens/Clients'
-import Contact from '../screens/Contact'
+import { Link } from 'react-router-dom'
+
 
 function Header() {
     return (
-        <div>
-        <Routes>
-            <Route path='/' element={<Home />}>Glenn Dutton</Route>
-            <Route path='/testimonials' element={<Testimonials />}>Testimonials</Route>
-            <Route path='/clients' element={<Clients />}>Clients</Route>
-            <Route path='/contact' element={<Contact />}>Contact</Route>
-        </Routes>
-        </div>
+        <nav>
+            <Link to="/">Brooklyn Longhurst</Link>
+            <Link to="/testimonials">Testimonials</Link>   
+            <Link to="/clients">Clients</Link>    
+            <Link to="/contact">Contact</Link>
+        </nav>
     )
 }
 
