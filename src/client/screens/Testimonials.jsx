e5adc98f5ee65738558efe80f203542de9dee8af
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-06-15 23:40:21 -0600

    adds testimonials

--- src/client/screens/Testimonials.jsx
+++ src/client/screens/Testimonials.jsx
@@ -4,6 +4,21 @@ function Testimonials() {
     return (
         <div>
             <h1>Testimonials Page</h1>
+            <p>
+                Glenn Dutton has worked with me over the past 15 years in presentations to corporate/organizational audiences ranging from 40 to 1000 participants throughout the country. He is an effective and personable teacher, kind and charismatic in working with people. Glenn walks his talk – and lives a centered and conscious life. Anyone who works with Glenn will love him and deeply appreciate his work.
+                - Tom C   
+                President, Aki Works, Inc.  
+            </p>
+            <p>     
+                Glenn is one of the most creative minds I have ever known. He knows his craft and he is a gifted mentor, teacher and friend. He has increased my effectiveness as a therapist and reduced my burnout.
+                - Charles H   
+                Therapist, Summit Counseling Services
+            </p>
+            <p>
+                Glenn Dutton has an incredible ability to deliver simple, powerful messages that engage a variety of audiences. He is phenomenal at drawing-in and connecting with people in a way that inspires and empowers them to look at confrontation in a new way. It is amazing to watch Glenn use his extensive background in martial arts and counseling to help professionals and families effectively navigate difficult situations and use negative energy in a positive and useful way. Perhaps what impresses me most about Glenn is his character. After getting to know Glenn over the past several months, I am increasingly amazed at how well he uses his talents to empower and inspire those around him. In addition to being an excellent speaker, I will never forget his kindness in reaching out to help a young college grad network and find a job that he now loves.
+                - Peter C   
+                Talent Strategy, Executive Search
+            </p>
         </div>
     )
 }
