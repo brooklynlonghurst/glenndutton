4897eef713e365434798e2127c62dc1e0b1b8299
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-06-16 15:22:49 -0600

    adds clients

--- src/client/screens/Clients.jsx
+++ src/client/screens/Clients.jsx
@@ -4,6 +4,26 @@ function Clients() {
     return (
         <div>
             <h1>Clients Page</h1>
+            <p>
+                American Express
+                Nextlink
+                The Walt Disney Company
+                Sony Pictures
+                Intermountain Healthcare
+                Make a Wish
+                Franklin Covey
+                Dupont
+                Union Pacific
+                Franklin Covey
+                Fidelity Investments
+                The Governor's Academy
+                The Church of Jesus Christ of Latter-day Saints
+                Church Education System
+                Miami Dolphins
+                Odyssey House Victoria
+                Utah Dental Association
+                U.S. Department of the Interior
+            </p>
         </div>
     )
 }
