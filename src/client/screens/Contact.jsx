818624a23b5c02eda7c61949b9351077f03153b9
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-06-19 11:41:17 -0600

    adds contact info

--- src/client/screens/Contact.jsx
+++ src/client/screens/Contact.jsx
@@ -4,6 +4,8 @@ function Contact() {
     return (
         <div>
             <h1>Contact Page</h1>
+            <p>Call or Text 801-243-6133</p>
+            <p>glenn@glenndutton.com</p>
         </div>
     )
 }
